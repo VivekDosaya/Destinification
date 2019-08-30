0666018c554e0299d06c955c62421d9d2e414be3
VivekD <vkdosaya@users.noreply.github.com>
2019-08-30 20:19:31 +0530

    Initial Commit

---
+++ Destinification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destinification
+//
+//  Created by Vivek Dosaya on 30/08/19.
+//  Copyright © 2019 Vivek Dosaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
