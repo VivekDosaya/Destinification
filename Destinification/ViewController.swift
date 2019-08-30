cb51065fc91090fd825fcc4bcbb341d44d41867e
VivekD <vkdosaya@users.noreply.github.com>
2019-08-30 20:32:10 +0530

    design completed, linking of components completed

--- Destinification/ViewController.swift
+++ Destinification/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stroryLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
 }
 
